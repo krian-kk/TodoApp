df3d83c4c631e367c6c81652e1921b0897f8048a
krian <krishnachouhan414@gmail.com>
2024-08-07 03:38:09 +0530

    added test_add_bloc.dart for testing add task feature

---
+++ test/mocks.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:todoapp/src/features/todo/domain/usecase/add_task_usecase.dart';
+
+@GenerateMocks([AddTask])
+void main()  {}
---
+++ test/test_add_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:todoapp/src/features/todo/bloc/add_task_bloc/add_task_bloc.dart';
+import 'package:todoapp/src/features/todo/domain/entities/task.dart';
+
+import 'mocks.mocks.dart';
+
+void main() {
+  late AddTaskBloc addTaskBloc;
+  late MockAddTask mockAddTask;
+
+  setUp(() {
+    mockAddTask = MockAddTask();
+    addTaskBloc = AddTaskBloc(addTask: mockAddTask);
+  });
+
+  test('initial state is AddTaskInitial', () {
+    expect(addTaskBloc.state, equals(AddTaskInitial()));
+  });
+
+  blocTest<AddTaskBloc, AddTaskState>(
+    'emits [TaskAdded] when AddTaskEvent is added',
+    build: () {
+      when(mockAddTask(any))
+          .thenAnswer((_) async => {});
+      return addTaskBloc;
+    },
+    act: (bloc) =>
+        bloc.add(AddTaskEvent(Task(id: 1, title: 'Test Task', isCompleted: 0))),
+    expect: () => [TaskAdded()],
+  );
+}
