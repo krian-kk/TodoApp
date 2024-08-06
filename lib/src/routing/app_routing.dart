1fe6423be4b409b7fe4dc5788127d66564621549
krian <krishnachouhan414@gmail.com>
2024-08-07 02:50:25 +0530

    routing added for task_home_screen.dart

--- lib/src/routing/app_routing.dart
+++ lib/src/routing/app_routing.dart
@@ -1,5 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todoapp/src/features/splash/presentation/splash_screen.dart';
+import 'package:todoapp/src/features/todo/bloc/add_task_bloc/add_task_bloc.dart';
+import 'package:todoapp/src/features/todo/bloc/get_task_bloc/get_task_bloc.dart';
+import 'package:todoapp/src/features/todo/data/repository/task_db_helper.dart';
+import 'package:todoapp/src/features/todo/data/repository/task_repository_impl.dart';
+import 'package:todoapp/src/features/todo/domain/usecase/add_task_usecase.dart';
 import 'package:todoapp/src/features/todo/presentation/pages/task_home_screen.dart';
 
 class AppRouter {
@@ -18,7 +24,17 @@ class AppRouter {
           GoRoute(
               path: '/taskHome',
               builder: (context, builder) {
-                return const TaskHomeScreen();
+
+                final taskDB = TaskDBHelper.instance;
+                final taskRepository = TaskRepositoryImpl(taskDB);
+
+                return MultiBlocProvider(providers: [
+                  BlocProvider(
+                      create: (context) => GetTasksBloc(taskRepository)),
+                  BlocProvider(
+                      create: (context) =>
+                          AddTaskBloc(addTask: AddTask(taskRepository))),
+                ], child: const TaskHomeScreen());
               }),
         ],
       );
