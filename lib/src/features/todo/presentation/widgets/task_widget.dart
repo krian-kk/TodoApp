805f41309bac45846d7b597d902a31664c48c663
krian <krishnachouhan414@gmail.com>
2024-08-08 06:02:38 +0530

    added logic for mark task and delete task in task_widget.dart

--- lib/src/features/todo/presentation/widgets/task_widget.dart
+++ lib/src/features/todo/presentation/widgets/task_widget.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/src/features/todo/bloc/delete_task_bloc/delete_task_bloc.dart';
+import 'package:todoapp/src/features/todo/bloc/update_task_bloc/update_task_bloc.dart';
 import 'package:todoapp/src/features/todo/domain/entities/task.dart';
 
 class TaskWidget extends StatelessWidget {
   final Task task;
+  final VoidCallback deleteCallback;
+  final VoidCallback toggleMarkCallback;
 
-  const TaskWidget({super.key, required this.task});
+  const TaskWidget(
+      {super.key,
+      required this.task,
+      required this.deleteCallback,
+      required this.toggleMarkCallback});
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +22,19 @@ class TaskWidget extends StatelessWidget {
       leading: Checkbox(
         value: task.isCompleted == 1,
         onChanged: (value) {
-
+          context.read<UpdateTaskCompletedBloc>().add(MarkTaskCompleteEvent(
+              Task(
+                  id: task.id,
+                  title: task.title,
+                  isCompleted: task.isCompleted)));
+          toggleMarkCallback();
         },
       ),
       trailing: IconButton(
-        icon: const Icon(Icons.delete),
+        icon: Icon(Icons.delete),
         onPressed: () {
-
+          context.read<DeleteTaskBloc>().add(DeleteTaskEvent(task.id));
+          deleteCallback();
         },
       ),
     );
